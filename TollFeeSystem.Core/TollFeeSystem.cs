a528e025b07f6d7a1aa6ffd85cfd18e3c0f53539
Henrik Bartholdsson <henrik.bartholdsson@hotmail.com>
2022-05-12 19:19:41 +0200

    Further structure and some thoughts.

---
+++ TollFeeSystem.Core/TollFeeSystem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TollFeeSystem.Core
+{
+    internal class TollFeeSystem
+    {
+        // Main class to interact with the system
+        // Use singleton
+        // Call portals with vehicle as arguments, the return should may be some kind of message.
+    }
+}
---
+++ TollFeeSystem.Core/VehicleRegistry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TollFeeSystem.Core
+{
+    public class VehicleRegistry
+    {
+        private void InformAuthorities() // May be in portal handler
+        {
+            Console.WriteLine("Unauthorized use of vehicle");
+        }
+    }
+}
