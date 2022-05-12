47516ab365434730dc1378fefc3d4c1c2325d60b
Henrik Bartholdsson <henrik.bartholdsson@hotmail.com>
2022-05-12 18:54:47 +0200

    Adding static data types for vehicle types.

---
+++ TollFeeSystem.Core/StaticData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TollFeeSystem.Core
+{
+    public static class StaticData
+    {
+        public enum VehicleType
+        {
+            Motorbike = 0,
+            Tractor = 1,
+            Emergency = 2,
+            Diplomat = 3,
+            Foreign = 4,
+            Military = 5
+        }
+    }
+}
